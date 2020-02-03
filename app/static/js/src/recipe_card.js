9f8f3e96907cad1ab5029b90d17d08a560e7ce53
lewis leighfield <ljleighfield@gmail.com>
2020-02-03 13:56:35 +0000

    Remove test React comps

--- app/static/js/src/recipe_card.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const RecipeCard = () => {
-    return <div>Does this work?</div>
-}
-
-let domContainer = document.querySelector('#react-recipe-card');
-ReactDOM.render(<RecipeCard />, domContainer);
