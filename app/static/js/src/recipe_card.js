4b6b5bbe1cfc17bde0e62ed4edfa1e21a753252e
lewis leighfield <ljleighfield@gmail.com>
2020-02-03 09:47:52 +0000

    Add React component func and babel preprocess

---
+++ app/static/js/src/recipe_card.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const RecipeCard = () => {
+    return <div>Does this work?</div>
+}
+
+let domContainer = document.querySelector('#react-recipe-card');
+ReactDOM.render(<RecipeCard />, domContainer);
