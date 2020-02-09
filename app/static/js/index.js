529910f00d9a26a1c4be1eee9791f7f206b87eb1
lewis leighfield <ljleighfield@gmail.com>
2020-02-09 11:08:57 +0000

    Change recipe del. butt. id to class to fix js function

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,7 +1,7 @@
 //Popup confirm box to ensure user actually wnts to delete recipe
 //redirects to the deleterecipe api using the recipes _id
 $(document).ready(() => {
-  $("#recipe-delete-confirm").on("click", e => {
+  $(".recipe-delete-confirm").on("click", e => {
     recipeId = $(e.target).data("id");
     if (confirm("Are you sure you want to delete this recipe?")) {
       location.href = `/deleterecipe/${recipeId}`;
