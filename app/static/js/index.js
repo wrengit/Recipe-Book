6a5e54e5a0216e0905b705c78e23fc98011946e1
lewis leighfield <ljleighfield@gmail.com>
2020-02-12 09:00:11 +0000

    add filter recipes by tags

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -7,4 +7,39 @@ $(document).ready(() => {
       location.href = `/deleterecipe/${recipeId}`;
     }
   });
+
+  $("#filter-check input").on("click", () => {
+    tags = [];
+
+    if ($("#filter-check :checkbox:checked").length < 0) {
+      $(".filter-div").show();
+    }
+
+    $("#filter-check :checkbox:checked").each(function() {
+      tags.push($(this).val());
+    });
+    $("#filter-check :checkbox:checked").each(function() {
+      tags.filter(tag => tag !== $(this).val());
+    });
+
+    function checkTags(div) {
+      let tagsArray = $(div)
+        .attr("data-tags")
+        .trim()
+        .split(" ");
+      if (
+        //https://stackoverflow.com/questions/9204283/how-to-check-whether-multiple-values-exist-within-an-javascript-array
+        tags.every(val => tagsArray.includes(val))
+      ) {
+        $(div).show();
+      } else {
+        $(div).hide();
+      }
+    }
+
+    const divsList = $(".filter-div");
+    [...divsList].forEach(div => {
+      checkTags(div);
+    });
+  });
 });
