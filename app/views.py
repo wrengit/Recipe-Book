ab8eea7b8e3db144145ca4718fa81f94af7c0b6f
lewis leighfield <ljleighfield@gmail.com>
2020-02-21 11:26:54 +0000

    add comments to views.py

--- app/views.py
+++ app/views.py
@@ -7,7 +7,9 @@ from app.forms import UserLoginForm, UserRegistrationForm, RecipeForm, SearchFor
 from app.users import User
 from bson import ObjectId
 
-# Index. Populates index with all recipes in db
+# Index. Populates index with all recipes in db.
+# All routes require 'search_form', as this is located in the navbar
+# which renders on each page
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -17,7 +19,8 @@ def index():
         return redirect(url_for('search_results', ingredient=search_form.search.data))
     return render_template('index.html', recipes=recipes, search_form=search_form, title='Recipe Book')
 
-# Login
+# Login route. If user is logged in then redirects to the index page. Uses the 'User'
+# model from 'users.py' to login with flask-login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -30,6 +33,10 @@ def login():
             user_obj = User(user['username'], user['email'],
                             user['_id'], user['is_admin'])
             login_user(user_obj)
+            # accesses the 'next page' query string to determine which url user wanted to visit
+            # before being redirected to the login page. If no next page was given then redirects user
+            # to the index page. 'url_parse.netloc' prevents malicious redirect attacks. This prevents
+            #redirects by ensuring that the url is relative to the page. 
             next_page = request.args.get('next')
             if not next_page or url_parse(next_page).netloc != '':
                 next_page = url_for('index')
@@ -38,14 +45,18 @@ def login():
             flash('Wrong username or password', 'warning')
     return render_template('loginform.html', form=form, search_form=search_form, title='Login')
 
-# Logout
+# Logout route. Logs user out of the session managemnt provided by Flask-login and redirects
+# logged out use to the index page. @login_required is a func from Flask-login which prevents
+# users accessing specific locations. Redirects users to the login page.
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-# Register new user
+# Register new user.
+# Writes a new user to the 'users' collection in the DB. This sets the user 'is_admin' boolean
+# to false for each new user. The only way to change this value is directly in the DB.
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -64,7 +75,9 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form, search_form=search_form, title='Register')
 
-# Profile route, displays recipes on profile page owned by user
+# Profile route, displays recipes on profile page owned by user.
+# 'recipes' shows all recipes which the user owns, and 'like_recipes' shows all recipes
+# that the user has liked.
 @app.route('/profile/<username>')
 def profile(username):
     search_form = SearchForm()
@@ -73,6 +86,7 @@ def profile(username):
     return render_template('profile.html', recipes=recipes, search_form=search_form, liked_recipes=liked_recipes, title='Profile')
 
 # Post recipe
+# posts a new recipes to the 'recipes' DB col.
 @app.route('/postrecipe', methods=['GET', 'POST'])
 @login_required
 def postrecipe():
@@ -94,6 +108,8 @@ def postrecipe():
     return render_template('add_recipe.html', form=form, search_form=search_form, title='Post Recipe')
 
 # Delete a recipe
+# uses the recipes 'id' to delete a recipe from the recipes col. Users must be logged in to view this route
+# Users may only delete a recipe if their username matches the owner field on the recipe.
 @app.route('/deleterecipe/<id>')
 @login_required
 def delete(id):
@@ -107,6 +123,8 @@ def delete(id):
     return redirect(url_for('index'))
 
 # Edit recipe
+# Allows user to edit their recipe. Ensure only logged in users can access and that their username
+# matches the owner field in recipe. This view preopulates the form with the current recipes info.
 @app.route('/editrecipe/<id>', methods=['GET', 'POST'])
 @login_required
 def editrecipe(id):
@@ -144,6 +162,9 @@ def editrecipe(id):
 
 
 # Search for ingredients
+# Performs a full text search of the ingredietns index on the recipes col. If a text match is found
+# return the results as 'recipes'. If not match is found, directs user to 'non_found.html' which
+# tells user that no matches were found.
 @app.route('/search_results/<ingredient>', methods=['GET', 'POST'])
 def search_results(ingredient):
     search_form = SearchForm()
@@ -156,7 +177,13 @@ def search_results(ingredient):
         return redirect(url_for('search_results', ingredient=search_form.search.data))
     return redirect(url_for('index'))
 
-
+#Like route
+# Allows user to 'like' a recipe. This adds the users 'username' to an array on the recipe in recipes col.
+# Route checks that the users username is in the array. If already in the array, this route wil remove the 
+# users username from the array. This ensures that users cannot 'like' a recipe multiple times, artifically
+# increasing the like count. This route is access through an AJAZ call in JS, and returns a json reply,
+# instead of a url redirect. This ensures the page does no reload, navigating the user away from the recipe
+# card.
 @app.route('/like/<id>', methods=['GET', 'POST'])
 def like(id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(id)})
